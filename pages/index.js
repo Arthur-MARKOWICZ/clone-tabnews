479ce2d56375eddcd19877f96810a5d0651c8cff
Your Name <arthurlopes25072005@gmail.com>
2025-10-27 14:30:42 -0300

    feat: arrumando a frase

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
 function Home() {
-  return <h1>Voce e uma pessoa muito especial </h1>;
+  return (
+    <div>
+      <h1>Voce e uma pessoa muito especial </h1> <h2>tenha um otimo dia</h2>
+    </div>
+  );
 }
 export default Home;
